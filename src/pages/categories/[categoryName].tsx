f1b1eab5a985cb0e5d0242ad67ee7933991b6d64
partha <spartha343@gmail.com>
2024-11-20 13:33:09 +0600

    second commit

--- src/pages/categories/[categoryName].tsx
+++ src/pages/categories/[categoryName].tsx
@@ -12,7 +12,7 @@ const CategoryPage = ({ products }: { products: IProduct[] }) => {
         Featured category Products
       </h2>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
-        {products.map((product) => (
+        {products?.map((product) => (
           <FeaturedProductCard key={product._id} product={product} />
         ))}
       </div>
