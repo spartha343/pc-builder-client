dbb4142774ad5111e5eedfc7fb44679526b0bb20
partha <spartha343@gmail.com>
2024-11-21 00:25:52 +0600

    categoryname component changed

--- src/pages/categories/[categoryName].tsx
+++ src/pages/categories/[categoryName].tsx
@@ -12,9 +12,10 @@ const CategoryPage = ({ products }: { products: IProduct[] }) => {
         Featured category Products
       </h2>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
-        {products?.map((product) => (
-          <FeaturedProductCard key={product._id} product={product} />
-        ))}
+        {products.length > 0 &&
+          products?.map((product) => (
+            <FeaturedProductCard key={product._id} product={product} />
+          ))}
       </div>
     </section>
   );
